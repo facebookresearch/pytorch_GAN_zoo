04f7dc19ade1df858bd12108176af0629a0eea82
Molugan <mriviere@fb.com>
2019-01-22 08:50:09 -0800

    BUGFIX: laplacian SWD

--- models/eval/laplacian_SWD.py
+++ models/eval/laplacian_SWD.py
@@ -1,130 +1,150 @@
 import os
-import sys
 import json
 
 import time
 
 import torch
-import torch.nn as nn
-import torchvision
 import torchvision.transforms as Transforms
 
-import torch.nn.functional as F
-
-import numpy as np
-import scipy.misc
-
-from ..metrics.laplacian_swd import LaplacianSWDMetric, getDescriptorsForMinibatch
-from ..progressive_gan import ProgressiveGAN
+from ..metrics.laplacian_swd import LaplacianSWDMetric
 from ..utils.utils import printProgressBar
 from ..datasets.attrib_dataset import AttribDataset
 from ..datasets.hd5 import H5Dataset
-from ..utils.utils import getVal, loadmodule, getLastCheckPoint, parse_state_name, getNameAndPackage
+from ..utils.utils import getVal, loadmodule, getLastCheckPoint, \
+                          parse_state_name, getNameAndPackage
 from ..utils.image_transform import NumpyResize
 
-def test(parser, visualisation = None):
 
+def test(parser, visualisation=None):
+
+    parser.add_argument('--selfNoise', action='store_true',
+                        help="Compute the inner noise of the dataset")
     kwargs = vars(parser.parse_args())
 
-    # Parameters
-    name =  getVal(kwargs,"name", None)
-    if name is None:
+    # Are all parameters available ?
+    name = getVal(kwargs, "name", None)
+    if name is None and not kwargs['selfNoise']:
         raise ValueError("You need to input a name")
 
-    module = getVal(kwargs,"module", None)
+    module = getVal(kwargs, "module", None)
     if module is None:
         raise ValueError("You need to input a module")
 
-    trainingConfig = getVal(kwargs,"config", None)
+    trainingConfig = getVal(kwargs, "config", None)
     if trainingConfig is None:
         raise ValueError("You need to input a configuration file")
 
-    #Mandatory fields
-    scale              = getVal(kwargs, "scale", None)
-    iter               = getVal(kwargs, "iter", None)
+    # Loading the model
+    scale = getVal(kwargs, "scale", None)
 
-    checkPointDir      = os.path.join(kwargs["dir"], modelLabel)
-    checkpointData     = getLastCheckPoint(checkPointDir, name, scale = scale, iter = iter)
+    if name is not None:
+        iter = getVal(kwargs, "iter", None)
 
-    if checkpointData is None:
-        print(scale, iter)
-        if scale is not None or iter is not None:
-            raise FileNotFoundError("Not checkpoint found for model " + name \
-                                    + " at directory " + dir + " for scale " + \
-                                    str(scale) + " at iteration " + str(iter))
-        raise FileNotFoundError("Not checkpoint found for model " + name + " at directory " + dir)
+        checkPointDir = os.path.join(kwargs["dir"], name)
+        checkpointData = getLastCheckPoint(
+            checkPointDir, name, scale=scale, iter=iter)
 
+        if checkpointData is None:
+            print(scale, iter)
+            if scale is not None or iter is not None:
+                raise FileNotFoundError("Not checkpoint found for model "
+                                        + name + " at directory " + dir +
+                                        " for scale " + str(scale) +
+                                        " at iteration " + str(iter))
+            raise FileNotFoundError(
+                "Not checkpoint found for model " + name + " at directory "
+                + dir)
 
-    modelConfig, pathModel, _ = checkpointData
+        modelConfig, pathModel, _ = checkpointData
+        with open(modelConfig, 'rb') as file:
+            configData = json.load(file)
 
-    if scale is None or iter is None:
-        _, scale, iter = parse_state_name(pathModel)
+        modelPackage, modelName = getNameAndPackage(module)
+        modelType = loadmodule(modelPackage, modelName)
 
+        model = modelType(useGPU=True,
+                          storeAVG=True,
+                          **configData)
+
+        if scale is None or iter is None:
+            _, scale, iter = parse_state_name(pathModel)
+
+        print("Checkpoint found at scale %d, iter %d" % (scale, iter))
+        model.load(pathModel)
+
+    elif scale is None:
+        raise AttributeError("Please provide a scale to compute the noise of \
+        the dataset")
+
+    # Building the score instance
+    depthPyramid = min(scale, 4)
+    SWDMetric = LaplacianSWDMetric(7, 128, depthPyramid)
+
+    # Building the dataset
     with open(trainingConfig, 'rb') as file:
         wholeConfig = json.load(file)
 
-    pathPartition      = wholeConfig.get("pathPartition", None)
-    partitionValue     = wholeConfig.get("partitionValue", None)
+    pathPartition = wholeConfig.get("pathPartition", None)
+    partitionValue = wholeConfig.get("partitionValue", None)
 
     partitionValue = getVal(kwargs, "partition_value", None)
 
-    #Training dataset properties
+    # Training dataset properties
     pathDB = wholeConfig["pathDB"]
     attribDict = wholeConfig.get('pathAttrib', None)
     size = 2**(2 + scale)
-    db_transform = Transforms.Compose([NumpyResize(size),
+    db_transform = Transforms.Compose([NumpyResize((size, size)),
                                        Transforms.ToTensor(),
-                                       Transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                                       Transforms.Normalize((0.5, 0.5, 0.5),
+                                                            (0.5, 0.5, 0.5))])
 
     if os.path.splitext(pathDB)[1] == '.h5':
         dataset = H5Dataset(pathDB,
-                            transform = db_transform,
-                            partition_path = pathPartition,
-                            partition_value = partitionValue)
+                            transform=db_transform,
+                            partition_path=pathPartition,
+                            partition_value=partitionValue)
     else:
         dataset = AttribDataset(pathdb=pathDB,
-                                transform = db_transform,
-                                attribDictPath = attribDict)
+                                transform=db_transform,
+                                attribDictPath=attribDict)
 
+    # Metric parameters
     nImagesSampled = min(len(dataset), 16000)
     batchSize = 16
     maxBatch = nImagesSampled / batchSize
-    depthPyramid = min(scale, 4)
 
-    dbLoader = torch.utils.data.DataLoader(dataset, batch_size = batchSize,
-                                           num_workers =2, shuffle=False)
+    dbLoader = torch.utils.data.DataLoader(dataset, batch_size=batchSize,
+                                           num_workers=2, shuffle=True)
 
-    with open(modelConfig, 'rb') as file:
-        configData = json.load(file)
+    if kwargs['selfNoise']:
 
-    modelPackage, modelName = getNameAndPackage(module)
-    modelType = loadmodule(modelPackage, modelName)
+        print("Computing the inner noise of the dataset...")
+        loader2 = torch.utils.data.DataLoader(dataset, batch_size=batchSize,
+                                              num_workers=2, shuffle=True)
 
-    model = modelType(useGPU = True,
-                      storeAVG = True,
-                      **configData)
+        for item, data in enumerate(zip(dbLoader, loader2)):
 
-    print("Checkpoint found at scale %d, iter %d" % (scale, iter))
+            if item > maxBatch:
+                break
 
-    SWDMetric = LaplacianSWDMetric(7, 128, depthPyramid)
-    model.load(pathModel)
+            real, fake = data
+            SWDMetric.updateWithMiniBatch(real[0], fake[0])
+            printProgressBar(item, maxBatch)
 
-    print("Generating the fake dataset...")
-    for item, data in enumerate(dbLoader, 0):
+    else:
 
-        if item > maxBatch:
-            break
+        print("Generating the fake dataset...")
+        for item, data in enumerate(dbLoader, 0):
 
-        inputsReal, labels = data
-        inputFake = model.test(model.buildNoiseData(inputsReal.size(0))[0], toCPU = False, getAvG = True)
+            if item > maxBatch:
+                break
 
-        if inputFake.size(3) < inputsReal.size(3):
-            ratio = int(inputsReal.size(3) / inputFake.size(3))
-            inputFake = F.upsample(inputFake, scale_factor = ratio, mode = 'nearest')
+            inputsReal, _ = data
+            inputFake = model.test(model.buildNoiseData(
+                inputsReal.size(0))[0], toCPU=False, getAvG=True)
 
-        SWDMetric.updateWithMiniBatch(inputFake, inputsReal)
-
-        printProgressBar(item, maxBatch)
+            SWDMetric.updateWithMiniBatch(inputFake, inputsReal)
+            printProgressBar(item, maxBatch)
 
     printProgressBar(maxBatch, maxBatch)
     print("Merging the results, please wait it can take some time...")
@@ -154,7 +174,7 @@ def test(parser, visualisation = None):
     outResults[str(scale)][str(iter)] = score
 
     with open(outPath, 'w') as file:
-        json.dump(outResults, file, indent = 2)
+        json.dump(outResults, file, indent=2)
 
     os.remove(flagPath)
 
