f138f12c94aaa42d5b38f20c86e77926d00f44dd
Camille Couprie <coupriec@devfair042.maas>
2019-02-24 23:09:55 -0800

    added missing sh files

---
+++ launch_SWD_comput2.sh
@@ -0,0 +1,10 @@
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 87000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 88000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 89000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 90000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 91000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 92000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 93000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 94000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5 -i 95000;
+python eval.py laplacian_SWD -c config_celeba.json -n default -m PGAN -d pgan16_celeba/ -s 5
---
+++ launch_celeba_camille.sh
@@ -0,0 +1,14 @@
+module load anaconda3
+module load NCCL/2.2.13-cuda.9.0 
+module load anaconda3 
+source activate fair_env_latest_py3
+
+pip3 install -r requirements.txt
+#export PYTHONPATH=/private/home/oteytaud/NEVERGRAD:$PYTHONPATH
+export PYTHONPATH=/private/home/coupriec/Riviere2018Fashion/pytorch_GAN_zoo:$PYTHONPATH
+
+python nevergrad/Test_inspiration-celebA.py | tee rescelebauls_`date | sed 's/ /_/g'`
+
+./viewer_celeb_camille.sh
+
+
