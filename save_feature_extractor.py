c0a96e0ad86fbd8cc5bf62aad9ae020b4c997180
Molugan <mriviere@fb.com>
2019-06-13 05:22:10 -0700

    REFACTOR: removing the inception features

--- save_feature_extractor.py
+++ save_feature_extractor.py
@@ -1,18 +1,19 @@
 import torch
 import argparse
-import torchvision
 from models.loss_criterions.loss_texture import LossTexture
-from models.networks.constant_net import MeanStd
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Testing script')
     parser.add_argument('model_name', type=str,
+                        choices=["vgg19", "vgg16"],
                         help="""Name of the desured featire extractor:
                         - vgg19, vgg16 : a variation of the style transfer \
                         feature developped in \
                         http://arxiv.org/abs/1703.06868""")
-    parser.add_argument('--layers', type=int, nargs='*', default=None)
+    parser.add_argument('--layers', type=int, nargs='*',
+                        help="For vgg models only. Layers to select. \
+                        Default ones are 3, 4, 5.", default=None)
     parser.add_argument('output_path', type=str,
                         help="""Path of the output feature extractor""")
 
@@ -25,13 +26,5 @@ if __name__ == "__main__":
                                        args.model_name,
                                        args.layers)
         featureExtractor.saveModel(args.output_path)
-    elif args.model_name == "inception":
-        model = torchvision.models.inception_v3(pretrained=True)
-        model.AuxLogits.fc = MeanStd()
-        model.fc = MeanStd()
-        mean = [2*p - 1 for p in[0.485, 0.456, 0.406]]
-        std = [2*p for p in [0.229, 0.224, 0.225]]
-        torch.save(dict(model=model, fullDump=True,
-                        mean=mean, std=std), args.output_path)
     else:
         raise AttributeError(args.model_name + " not implemented yet")
