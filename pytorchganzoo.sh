5c3514e81aa0784a0e3f4865a0f2f395e96a09dd
Olivier Teytaud <oteytaud@fb.com>
2019-09-19 12:48:11 +0200

    ot

--- pytorchganzoo.sh
+++ pytorchganzoo.sh
@@ -9,18 +9,21 @@ OUTPUT_DATASET=celeba_output
 
 CONFIGURATION_FILE=config_celeba_cropped.json
 
+#TODO Run this the first time (and only the first time):
 #python3.6 datasets.py celeba_cropped $PATH_TO_CELEBA -o $OUTPUT_DATASET
 
 
-alphaJumpMode linear|custom
-initBiasToZero True|False
-perChannelNormalization True|False
-#leakyness 0.2
-#epsilonD 0.001
-miniBatchStdDev False|True
-#baseLearningRate 0.001
+#alphaJumpMode linear|custom
+#initBiasToZero True|False
+#perChannelNormalization True|False
+##leakyness 0.2
+##epsilonD 0.001
+#miniBatchStdDev False|True
+##baseLearningRate 0.001
 
 
+#TODO xp with oneshot methods
+#TODO add parameters around the structure of the gan
 lr=`./paramcloseto0.sh 0.00001 0.1 $1`  # first parameter for learning rate
 ed=`./paramcloseto0.sh 0.00001 0.1 $2`  # first parameter for epsilond
 ln=`./paramcloseto0.sh 0.01 0.6 $3`  # first parameter for epsilond
@@ -30,3 +33,4 @@ python3.6 train.py PGAN -c $CONFIGURATION_FILE  --restart -n celeba_output --no_
 # 
 # python eval.py laplacian_SWD -c $CONFIGURATION_FILE -n $modelName -m $modelType
 
+#TODO: return the result
