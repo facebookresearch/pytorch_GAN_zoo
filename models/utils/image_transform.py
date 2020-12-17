b2144209a3a05d8bb256e47c9783e73739a25472
Robert W Rankin <robertw.rankin@gmail.com>
2020-12-17 13:24:33 -0500

    edits for albumentations

--- models/utils/image_transform.py
+++ models/utils/image_transform.py
@@ -4,11 +4,51 @@ import torchvision.transforms as Transforms
 import os
 import random
 import numpy as np
+import torch
+import albumentations as A
+import cv2
+
 from PIL import Image
+
+
 
 # The equivalent of some torchvision.transforms operations but for numpy array
 # instead of PIL images
-
+class ToTensorV2(A.BasicTransform):
+    """Convert image and mask to `torch.Tensor`.
+    Args:
+        transpose_mask (bool): if True and an input mask has three dimensions, this transform will transpose dimensions
+        so the shape `[height, width, num_channels]` becomes `[num_channels, height, width]`. The latter format is a
+        standard format for PyTorch Tensors. Default: False.
+    """
+    
+    def __init__(self, transpose_mask=False, always_apply=True, p=1.0):
+        super(ToTensorV2, self).__init__(always_apply=always_apply, p=p)
+        self.transpose_mask = transpose_mask
+    
+    @property
+    def targets(self):
+        return {"image": self.apply, "mask": self.apply_to_mask}
+    
+    def apply(self, img, **params):  # skipcq: PYL-W0613
+        if len(img.shape) not in [2, 3]:
+            raise ValueError("Albumentations only supports images in HW or HWC format")
+        
+        if len(img.shape) == 2:
+            img = np.expand_dims(img, 2)
+        
+        return torch.from_numpy(img.transpose(2, 0, 1))
+    
+    def apply_to_mask(self, mask, **params):  # skipcq: PYL-W0613
+        if self.transpose_mask and mask.ndim == 3:
+            mask = mask.transpose(2, 0, 1)
+        return torch.from_numpy(mask)
+    
+    def get_transform_init_args_names(self):
+        return ("transpose_mask",)
+    
+    def get_params_dependent_on_targets(self, params):
+        return {}
 
 class NumpyResize(object):
 
@@ -70,18 +110,34 @@ class NumpyToTensor(object):
         return Transforms.functional.to_tensor(img)
 
 
-def pil_loader(path):
+#def pil_loader(path):
+#    imgExt = os.path.splitext(path)[1]
+#    if imgExt == ".npy":
+#        img = np.load(path)[0]
+#        return np.swapaxes(np.swapaxes(img, 0, 2), 0, 1)
+#
+#    # open path as file to avoid ResourceWarning
+#    # (https://github.com/python-pillow/Pillow/issues/835)
+#    with open(path, 'rb') as f:
+#        img = Image.open(f)
+#        return img.convert('RGB')
+
+def pil_loader(path, return_np=None):
+    if return_np is None:
+        return_np = False
     imgExt = os.path.splitext(path)[1]
     if imgExt == ".npy":
         img = np.load(path)[0]
         return np.swapaxes(np.swapaxes(img, 0, 2), 0, 1)
-
+    
     # open path as file to avoid ResourceWarning
     # (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
-        return img.convert('RGB')
-
+        if return_np:
+            return np.array(img.convert('RGB'))
+        else:
+            return img.convert('RGB')
 
 def standardTransform(size):
     return Transforms.Compose([NumpyResize(size),
